67b3d9d658d5efa21f56f18d8d7a31f7d93615bb
Lee Dong Eun <sixgen@gmail.com>
2022-04-07 17:14:08 +0900

    rm gom

--- test/File-await.js
+++ test/File-await.js
@@ -1,18 +1,15 @@
-import { loadFile } from '../src/file-tool.js'
+import { loadFile, File } from '../src/file-tool.js'
 import { Blob } from 'buffer'
 
 
 const lock = await loadFile('lock.jpg')
-const host = await loadFile('host.jpg')  
-const gom = await loadFile('gom.jpg') 
+const host = await loadFile('host.jpg')
 
-const files = [lock, host, gom]
+const files = [lock, host]
 
-console.log( files ) 
-console.log( lock.name ) 
-console.log( lock.slice(0,20) ) 
-console.log( await lock.arrayBuffer(0,20) ) 
-
-
-console.log( lock.constructor )
-console.log( lock instanceof Blob )
+console.log(files)
+console.log(lock.name)
+console.log(lock.slice(0, 20))
+console.log(await lock.arrayBuffer(0, 20))
+console.log(lock instanceof Blob)
+console.log(lock instanceof File)
